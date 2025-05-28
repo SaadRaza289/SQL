f876915327efd9d4972e0c4caef5f01a5642ae6b
SaadRaza289 <120735454+SaadRaza289@users.noreply.github.com>
2025-05-28 23:27:52 +0500

    Add files via upload

---
+++ bronze_layer.sql
@@ -0,0 +1,13 @@
+-- Data is loaded using python script
+
+SELECT * FROM crm_cust_info;
+
+SELECT * FROM crm_prd_info;
+
+SELECT * FROM crm_sales_details;
+
+SELECT * FROM "erp_CUST_AZ12";
+
+SELECT * FROM "erp_LOC_A101";
+
+SELECT * FROM "erp_PX_CAT_G1V2";
